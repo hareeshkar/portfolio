5ff29e84cb58b0fea6156abf56dcd3df8c2f54dc
Hareeshkar Ravi <hareeshkarravi@Hareeshkars-MacBook-Air.local>
2025-11-21 16:07:30 +0530

    Mobile Optimiztion V3.1 Optimized Conatac component lazy loading error

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/portfolio/", // This matches your GitHub repository name
+  base: "/portfolio/", // Required for GitHub Pages to serve assets correctly
 });
